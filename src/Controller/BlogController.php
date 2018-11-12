6613d30590332677cc4ddd6b8183ee7dd1bd9651
Julien Delbez <julien.delbez@hotmail.fr>
2018-11-12 14:20:00 +0100

    fin de quete param converter

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,19 +2,27 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Category;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\ArticleSearchType;
+
+
+/**
+ * Class BlogController
+ * @package App\Controller
+ */
 class BlogController extends AbstractController
 {
     /**
-     * @Route("/blog/{slug}", requirements={"slug" = "[a-z0-9-]+"}, name="blog")
+     * @Route("/blog/{id}", name="category")
      */
-    public function show($slug = "Article Sans Titre")
+    public function show(Category $category): Response
     {
-        $slug = str_replace('-', ' ',$slug);
-        $slug = ucwords($slug);
-        return $this->render('blog/index.html.twig', ['slug' => $slug]);
+        return $this->render('blog/index.html.twig', ['category' => $category]);
     }
 }
-
---
+++ src/Form/CategoryType.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: julien
+ * Date: 12/11/18
+ * Time: 13:24
+ */
+
+namespace App\Form;
+
+
+class CategoryType
+{
+
+}
