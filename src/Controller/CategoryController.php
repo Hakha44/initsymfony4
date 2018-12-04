2f6347b07c15050bd175aad7841cb2190fb2e949
Julien Delbez <julien.delbez@hotmail.fr>
2018-12-04 10:43:04 +0100

    correction relation bidirectionnelle

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -12,25 +12,38 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
+//    /**
+//     * @Route("/form")
+//     */
+//    public function index()
+//    {
+//        $category = new Category();
+//        $form = $this->createForm(CategoryType::class, $category);
+//
+//        return $this->render(
+//            'form/index.html.twig', [
+//                'form' => $form->createView(),
+//            ]
+//        );
+//    }
+
     /**
-     * @Route("/form")
+     * @Route("category", name="category")
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function index()
     {
-        $category = new Category();
-        $form = $this->createForm(CategoryType::class, $category);
-
+        $categories = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findAll();
+        if (!$categories) {
+            throw $this->createNotFoundException(
+                'No article found in article\'s table.'
+            );
+        }
         return $this->render(
-            'form/index.html.twig', [
-                'form' => $form->createView(),
-            ]
+            '/category/index.html.twig',
+            ['categories' => $categories]
         );
-    }
-    /**
-     * @Route("/category/{id}", name="category")
-     */
-    public function show(Category $category): Response
-    {
-        return $this->render('category/index.html.twig', ['category' => $category]);
     }
 }
